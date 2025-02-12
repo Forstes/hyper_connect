c639e1d7306843ab031b8dca07042ff606042c7d
forstes <b.rodion70@mail.ru>
2025-02-12 11:08:45 +0500

    feature gating

--- src/connectors/enums/connector.rs
+++ src/connectors/enums/connector.rs
@@ -1,14 +1,22 @@
 use super::send_request::SendRequestEnum;
-use crate::connectors::{
-    http1::SimpleHttp1Connector, http1_proxy::ProxyHttp1Connector, http2::SimpleHttp2Connector,
-    http2_proxy::ProxyHttp2Connector,
-};
+#[cfg(feature = "http1")]
+use crate::connectors::http1::SimpleHttp1Connector;
+#[cfg(feature = "http1_proxy")]
+use crate::connectors::http1_proxy::ProxyHttp1Connector;
+#[cfg(feature = "http2")]
+use crate::connectors::http2::SimpleHttp2Connector;
+#[cfg(feature = "http2_proxy")]
+use crate::connectors::http2_proxy::ProxyHttp2Connector;
 use hyper::{body::Body, Uri};
 
 pub enum ConnectorEnum {
+    #[cfg(feature = "http1")]
     Http1(SimpleHttp1Connector),
+    #[cfg(feature = "http1_proxy")]
     Http1Proxy(ProxyHttp1Connector),
+    #[cfg(feature = "http2")]
     Http2(SimpleHttp2Connector),
+    #[cfg(feature = "http2_proxy")]
     Http2Proxy(ProxyHttp2Connector),
 }
 
@@ -20,13 +28,17 @@ impl ConnectorEnum {
         B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
     {
         match self {
+            #[cfg(feature = "http1")]
             ConnectorEnum::Http1(connector) => connector.create_connection(uri).await,
+            #[cfg(feature = "http1_proxy")]
             ConnectorEnum::Http1Proxy(connector) => {
                 connector
                     .create_connection(uri, &connector.proxy_address, &connector.username, &connector.password)
                     .await
             }
+            #[cfg(feature = "http2")]
             ConnectorEnum::Http2(connector) => connector.create_connection(uri).await,
+            #[cfg(feature = "http2_proxy")]
             ConnectorEnum::Http2Proxy(connector) => {
                 connector
                     .create_connection(uri, &connector.proxy_address, &connector.username, &connector.password)
--- src/connectors/mod.rs
+++ src/connectors/mod.rs
@@ -1,5 +1,7 @@
 pub mod enums;
 pub mod http1;
+#[cfg(feature = "http1_proxy")]
 pub mod http1_proxy;
 pub mod http2;
+#[cfg(feature = "http2_proxy")]
 pub mod http2_proxy;
