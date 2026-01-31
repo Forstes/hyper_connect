9ed6eb79ce9707fcd113c1d60df22a5ec1a512ba
forstes <b.rodion70@mail.ru>
2026-01-31 20:14:11 +0500

    fix(connection): correct feature scoping

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -2,17 +2,20 @@ use hyper::body::{Body, Incoming};
 use std::future::Future;
 
 pub trait HttpConnection<B: Body> {
-    fn send_request(&mut self, req: hyper::Request<B>) -> impl Future<Output = Result<hyper::Response<Incoming>, hyper::Error>> + Send;
+    fn send_request(
+        &mut self,
+        req: hyper::Request<B>,
+    ) -> impl Future<Output = Result<hyper::Response<Incoming>, hyper::Error>> + Send;
     fn is_conn_ready(&self) -> bool;
     fn is_conn_closed(&self) -> bool;
 }
 
-#[cfg(feature = "http1")]
+#[cfg(any(feature = "http1", feature = "http1_proxy"))]
 pub struct Http1Connection<B: Body + 'static> {
     pub conn: hyper::client::conn::http1::SendRequest<B>,
 }
 
-#[cfg(feature = "http1")]
+#[cfg(any(feature = "http1", feature = "http1_proxy"))]
 impl<B: Body + Send + 'static> HttpConnection<B> for Http1Connection<B> {
     async fn send_request(&mut self, req: hyper::Request<B>) -> Result<hyper::Response<Incoming>, hyper::Error> {
         self.conn.send_request(req).await
@@ -27,12 +30,12 @@ impl<B: Body + Send + 'static> HttpConnection<B> for Http1Connection<B> {
     }
 }
 
-#[cfg(feature = "http2")]
+#[cfg(any(feature = "http2", feature = "http2_proxy"))]
 pub struct Http2Connection<B: Body + 'static> {
     pub conn: hyper::client::conn::http2::SendRequest<B>,
 }
 
-#[cfg(feature = "http2")]
+#[cfg(any(feature = "http2", feature = "http2_proxy"))]
 impl<B: Body + Send + 'static> HttpConnection<B> for Http2Connection<B> {
     async fn send_request(&mut self, req: hyper::Request<B>) -> Result<hyper::Response<Incoming>, hyper::Error> {
         self.conn.send_request(req).await
