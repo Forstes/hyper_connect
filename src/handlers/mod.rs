eed5f3d1d43454ae012bd391d261f919f60fb598
forstes <b.rodion70@mail.ru>
2026-08-05 15:54:01 +0500

    fix: gate protocol handlers by feature

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,6 +1,10 @@
+#[cfg(any(feature = "http1", feature = "http1_proxy"))]
 pub mod http1;
+#[cfg(any(feature = "http1", feature = "http1_proxy"))]
 pub mod http1_conn_pool;
+#[cfg(any(feature = "http2", feature = "http2_proxy"))]
 pub mod http2;
+#[cfg(any(feature = "http2", feature = "http2_proxy"))]
 pub mod http2_conn_pool;
 #[cfg(feature = "rate_limit")]
 pub mod rate_limiter;
